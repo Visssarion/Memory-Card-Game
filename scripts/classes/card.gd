f34e7ec625d85c09dd8ec2bc5639a8c3386d1339
Vissa <46628714+Visssarion@users.noreply.github.com>
2023-11-20 18:37:14 +0300

    Clicking on cards: detecting + signal

--- scripts/classes/card.gd
+++ scripts/classes/card.gd
@@ -2,4 +2,12 @@ extends Node2D
 
 class_name Card
 
+signal clicked(card : Card)
+
 var card_data: CardData
+
+## Recieves a signal if mouse interacts with a card.
+## Used to check if card was clicked
+func _on_area_2d_input_event(viewport, event : InputEventMouseButton, shape_idx):
+	if event.pressed == false: # false - button release
+		clicked.emit(self)
