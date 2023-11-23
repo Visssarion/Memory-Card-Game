57a0d7df7cc7d834fa3fdc44376f8b867b24c9c5
undeƒined <my.development.business@gmail.com>
2023-11-23 04:18:09 +0100

    FIX: Temporary fix to fit bigger board when scene switches

--- scripts/classes/game_camera_2d.gd
+++ scripts/classes/game_camera_2d.gd
@@ -7,25 +7,30 @@ extends Camera2D
 @export var out_of_board_pixel_amount : float = 40
 
 var _default_scale : float = 1
+var _controlled_scale: int = 1
+var rows: int = 2
 
 func update_scale(rows: int, pixelsize: int, gridsize: int):
-	var board_size = (pixelsize + gridsize) * rows + out_of_board_pixel_amount
+	var board_size = ((pixelsize + gridsize) * rows) + out_of_board_pixel_amount
 	_default_scale = default_resolution.y / board_size
+	_controlled_scale = board_size / 1000
+	rows = rows
 	_update_zoom()
 
 func _ready():
 	get_viewport().size_changed.connect(_update_zoom)
+	_update_zoom()
 
 func _update_zoom():
 	var viewport_size = get_viewport().get_visible_rect().size
 	# Calculates difference in scale compared to default resolution
 	var viewport_x_scale = viewport_size.x / default_resolution.x
 	var viewport_y_scale = viewport_size.y / default_resolution.y
-	
+
 	# bro i am so sorry.
 	# for more info, check this shit out \/
 	# https://user-images.githubusercontent.com/46628714/284697889-febe874f-32da-41d1-88dd-0df9624e21cb.png
 	if viewport_x_scale < viewport_y_scale: # empty space vertically
-		self.zoom = Vector2(viewport_x_scale, viewport_x_scale) * _default_scale
+		self.zoom = Vector2(viewport_x_scale, viewport_x_scale) * (_default_scale / 2 )  # No I'M SorRy BrUh
 	else: #                                 # empty space horizontally
-		self.zoom = Vector2(viewport_y_scale, viewport_y_scale) * _default_scale
+		self.zoom = Vector2(viewport_y_scale, viewport_y_scale) * (_default_scale / 2 )  # No I'M SorRy BrUh
